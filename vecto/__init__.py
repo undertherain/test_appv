1761a7dd97a2b6696494c4fc2ccacc7d8fe0310c
Aleksandr Drozd <alexander.drozd@gmail.com>
2019-01-06 22:18:25 +1000

    add vecto folder

---
+++ vecto/__init__.py
@@ -0,0 +1,14 @@
+"""vecto is a library for all things related to vector space models in NLP
+
+Submodules
+==========
+
+.. autosummary::
+    :toctree: _autosummary
+
+    embeddings
+    corpus
+    vocabulary
+    benchmarks
+"""
+
